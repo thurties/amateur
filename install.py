5a0cbd6d3dda423ba10efebcebc9af4e74aab033
thurties <30533963+thurties@users.noreply.github.com>
2018-11-22 15:57:54 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,34 @@
+import subprocess
+
+def cal(cmd):
+	subprocess.call(cmd, shell=True)
+
+print("Are You Using Centos or Debian?")
+print("If Centos, Enter [1]")
+print("If Debian, Enter [2]")
+print("")
+choice = raw_input("[root@admin]#")
+if choice == '1':
+ print("DOWNLOADING SCRIPTS!")
+
+ cal("yum update")
+ cal("yum install nmap")
+ cal("yum install git")
+ cal("mkdir files")
+ cal("pip install sockets")
+ cal("cd files && git clone https://github.com/dotfighter/torshammer")
+ cal("cd files && git clone https://github.com/jseidl/GoldenEye")
+ cal("clear")
+ print ("ALL FILES INSTALLED AND READY TO GO!")
+if choice == '2':
+  print("DOWNLOADING SCRIPTS!")
+
+  cal("apt-get update")
+  cal("apt-get install nmap")
+  cal("apt-get install git")
+  cal("mkdir files")
+  cal("pip install sockets")
+  cal("cd files && git clone https://github.com/dotfighter/torshammer")
+  cal("cd files && git clone https://github.com/jseidl/GoldenEye")
+  cal("clear")
+  print("ALL FILES INSTALLED AND READY TO GO!")
